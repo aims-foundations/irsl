50b0914e1596bd3abdf0088e610ca8e26c19c478
Yuheng Tu <yuhengtu@skampere1.stanford.edu>
2025-06-03 06:11:51 -0700

    new

--- monkey/monkey_gather_data/monkey_gather_rylan_query.py
+++ monkey/monkey_gather_data/monkey_gather_rylan_query.py
@@ -3,23 +3,24 @@ import os
 import json
 from datasets import load_dataset
 from tqdm import tqdm
-from datasets import concatenate_datasets
+import glob
+from huggingface_hub import HfApi
 
-dataset = "GSM8K"
-if dataset == "MATH":
-    configs = ['algebra', 'counting_and_probability', 'geometry', 'intermediate_algebra', 'number_theory', 'prealgebra', 'precalculus']
-    ds_list = [
-        load_dataset("EleutherAI/hendrycks_math", cfg, split="test")
-        for cfg in configs
-    ]
-    ds = concatenate_datasets(ds_list)
-    df = pd.read_parquet('data/models_math_evals_2024-10-10.parquet')
-    keyname = 'problem'
+# dataset = "GSM8K"
+# if dataset == "MATH":
+#     configs = ['algebra', 'counting_and_probability', 'geometry', 'intermediate_algebra', 'number_theory', 'prealgebra', 'precalculus']
+#     ds_list = [
+#         load_dataset("EleutherAI/hendrycks_math", cfg, split="test")
+#         for cfg in configs
+#     ]
+#     ds = concatenate_datasets(ds_list)
+#     df = pd.read_parquet('data/models_math_evals_2024-10-10.parquet')
+#     keyname = 'problem'
 
-elif dataset == "GSM8K":
-    ds = load_dataset("openai/gsm8k", "main", split="test")
-    # df = pd.read_parquet('../../data/models_gsm8k_evals_2024-10-10.parquet')
-    keyname = 'question'
+# elif dataset == "GSM8K":
+ds = load_dataset("openai/gsm8k", "main", split="test")
+df = pd.read_parquet('../../data/models_gsm8k_evals_2024-10-10.parquet')
+keyname = 'question'
 
 df300 = df[df['Model Nickname'].str.endswith('300B')].copy()
 model_names = sorted({
@@ -27,17 +28,15 @@ model_names = sorted({
     for n in df300['Model Nickname'].unique()
 })
 
-output_dir = "data/rylan_monkey"
+output_dir = "../../data/rylan_monkey"
 os.makedirs(output_dir, exist_ok=True)
 
 for model in tqdm(model_names):
     sel = df300[df300['Model Nickname'].str.startswith(model)]
     
     records = []
-    diff_lens = []
     for idx in sorted(sel['prompt_idx'].unique()):
-        scores = sel.loc[sel['prompt_idx'] == idx, 'Score'].tolist()[:10000]
-        diff_lens.append(len(sel.loc[sel['prompt_idx'] == idx, 'Score'].tolist()))
+        scores = sel.loc[sel['prompt_idx'] == idx, 'Score'].tolist()
         # if len(scores) == 128 and set(scores) == {0}:
         #     scores = [0] * 10000
         # elif len(scores) == 128 and set(scores) != {0}:
@@ -47,10 +46,20 @@ for model in tqdm(model_names):
         records.append({
             'is_corrects': scores,
             'question': question,
-            'prompt_idx': int(idx)
+            # 'prompt_idx': int(idx)
         })
-    print(model, set(diff_lens))
     
-    out_path = f'{dataset}_{model}.json'
+    out_path = f'{model}_gsm.json'
     with open(f"{output_dir}/{out_path}", 'w') as f:
         json.dump(records, f, indent=2)
+        
+    api = HfApi()
+    rylan_query_list = glob.glob("../../data/rylan_monkey/*gsm.json")
+    for file_path in rylan_query_list:
+        filename = os.path.basename(file_path)
+        api.upload_file(
+            path_or_fileobj=file_path,
+            path_in_repo=f"{filename}",
+            repo_id="stair-lab/monkey_queries",
+            repo_type="dataset",
+        )
