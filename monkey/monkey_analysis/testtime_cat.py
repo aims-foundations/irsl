19be48954383df711c74e4e80e2ce03f8a450857
sangttruong <sangttruong.real@gmail.com>
2025-09-19 19:31:57 -0700

    testtime law debug

--- monkey/monkey_analysis/testtime_cat.py
+++ monkey/monkey_analysis/testtime_cat.py
@@ -6,7 +6,6 @@ import torch
 torch.manual_seed(0)
 torch.set_num_threads(1)
 from tqdm import tqdm
-from collections import defaultdict
 from joblib import Parallel, delayed
 import sys
 sys.path.append("../..")
@@ -18,34 +17,68 @@ from scipy.stats import spearmanr
 import warnings
 warnings.filterwarnings("ignore")
 
+def compute_pass_iatk_gt(n: int, c: int, k: int) -> float:
+    if n - c < k:
+        return 1.0
+    return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
+
+def compute_pass_datk_gts(data2d: np.ndarray) -> np.ndarray:
+    n_items, n_samples = data2d.shape
+    k_range = np.arange(1, n_samples + 1)
+    per_item = []
+    for i in range(n_items):
+        arr = data2d[i]
+        valid = ~np.isnan(arr) # if data2d is torch.tensor, this line return a list of 255
+        n = int(valid.sum())
+        c = int(np.nansum(arr))
+        per_item.append([
+            compute_pass_iatk_gt(n, c, k)
+            for k in k_range
+        ])
+    return np.nanmean(np.vstack(per_item), axis=0)
+
+def compute_pass_iatk_irt(pass_iat1: float, k: int) -> float:
+    return 1.0 - (1.0 - pass_iat1) ** k
+
+def compute_pass_datk_irt(data2d: np.ndarray, irt_probs: np.ndarray) -> np.ndarray:
+    n_items, n_samples = data2d.shape
+    assert n_items == irt_probs.shape[0]
+    k_range = np.arange(1, n_samples + 1)
+    per_item = []
+    for i in range(n_items):
+        per_item.append([
+            compute_pass_iatk_irt(irt_probs[i], k)
+            for k in k_range
+        ])
+    return np.nanmean(np.vstack(per_item), axis=0)
+
 if __name__ == "__main__":
     device = "cpu" # "cuda:4"
     budget = 30
     max_workers = 16
     
-    cache_dir = snapshot_download(repo_id="stair-lab/irsl_testtime_resmat1", repo_type="dataset")
-    testtime_resmat1 = torch.load(f"{cache_dir}/testtime_resmat2.pt", map_location="cpu", weights_only=False)
-    data_tensor = testtime_resmat1["data_tensor"]
+    # cache_dir = snapshot_download(repo_id="stair-lab/irsl_testtime_resmat2", repo_type="dataset")
+    testtime_resmat2 = torch.load(f"irsl_testtime_resmat2_withz.pt", map_location="cpu", weights_only=False)
+    data_tensor = testtime_resmat2["data_tensor"].numpy()
     print(data_tensor.shape)
-    model_names = testtime_resmat1["models"]
-    benchmarks  = testtime_resmat1["benchmarks"]
-    scenarios   = testtime_resmat1["scenarios"]
-    questions   = testtime_resmat1["questions"]
-    zs = testtime_resmat1["zs"]
+    model_names = testtime_resmat2["models"]
+    datasets  = testtime_resmat2["datasets"]
+    questions   = testtime_resmat2["questions"]
+    zs = testtime_resmat2["zs"]
     
-    results_dict = defaultdict(lambda: defaultdict(dict))
-    for scen in tqdm(set(scenarios)):
+    for scen in tqdm(set(datasets)):
         output_dir = f"../../result/testtime_cat/{scen}"
         os.makedirs(output_dir, exist_ok=True)
-            
-        idxs = [j for j, s in enumerate(scenarios) if s == scen]
+
+        idxs = [j for j, s in enumerate(datasets) if s == scen]
         sub_tensor = data_tensor[:, idxs, :]
+        sub_zs = np.array(zs)[idxs]
         print(f"{scen}: shape = {sub_tensor.shape}")
-        resmat = sub_tensor[:, :, 0]
-        # visualize_response_matrix(resmat, resmat, f"{output_dir}/response_matrix.png")
-        sub_ys = torch.tensor(resmat, dtype=torch.float, device=device)
+        
+        sub_ys = sub_tensor[:, :, 0]
+        sub_ys = torch.tensor(sub_ys, dtype=torch.float, device=device)
         n_test_takers, n_items = sub_ys.shape
-        sub_zs = torch.tensor(np.array(zs)[idxs], dtype=torch.float, device=device)
+        sub_zs = torch.tensor(sub_zs, dtype=torch.float, device=device)
         
         # z distribution
         with plt.rc_context(bundles.icml2024(usetex=True, family="serif")):
@@ -80,17 +113,18 @@ if __name__ == "__main__":
             plt.close()
         
         for i, model in enumerate(model_names):
+            model_data = sub_tensor[i]
             # plot of irt_corr_passat1
-            irt_prob = torch.sigmoid(float(final_thetas[i]) + sub_zs).cpu().numpy()
-            # irt_prob = (float(final_thetas[i]) + sub_zs).cpu().numpy()
-            # irt_prob = sub_zs.cpu().numpy()
-            passat1 = sub_tensor.mean(-1)[i]
-            rho, pval = spearmanr(irt_prob, passat1, nan_policy="omit")
+            irt_probs = torch.sigmoid(float(final_thetas[i]) + sub_zs).cpu().numpy()
+            passat1 = np.nanmean(model_data, axis=-1)
+            rho, pval = spearmanr(irt_probs, passat1, nan_policy="omit")
             with plt.rc_context(bundles.icml2024(usetex=True, family="serif")):
                 plt.figure(figsize=(6, 6))
-                plt.scatter(irt_prob, passat1, s=10)
-                plt.xlabel(r"$\theta$ + $z$", fontsize=18)
+                plt.scatter(irt_probs, passat1, s=10)
+                plt.xlabel(r"$\sigma$($\theta$ + $z$)", fontsize=18)
                 plt.ylabel("pass@1", fontsize=18)
+                plt.xlim(0,1)
+                plt.ylim(0,1)
                 plt.title(rf"{model, scen} ($\rho$ = {rho:.2f})", fontsize=16)
                 plt.tick_params(axis="both", labelsize=14)
                 plt.tight_layout()
@@ -98,36 +132,20 @@ if __name__ == "__main__":
                 plt.close()
             
             # law curve
-            
-    #     step_pcts = steps.astype(int) / steps.astype(int).max() * 100.0
-    #     means_all = torch.nanmean(sub_ys, dim=1).cpu().numpy() # mean score on all questions
-    #     means_sub = torch.nanmean(sub_ys[:, torch.randperm(sub_ys.shape[1])[:budget]], dim=1).cpu().numpy() # mean score on random subset
-    #     with plt.rc_context(bundles.icml2024(usetex=True, family="serif")):
-    #         fig, ax1 = plt.subplots(figsize=(6, 4))
-    #         ax1.plot(step_pcts, means_sub, color="tab:blue", linewidth=1.5, label="random subset mean")
-    #         ax1.plot(step_pcts, means_all, color='tab:blue', linewidth=1.5, label="full set mean", linestyle="--")
-    #         ax1.set_xlabel(r"Training progress (\%)", fontsize=16)
-    #         ax1.set_ylabel("Mean score", color='tab:blue', fontsize=16)
-    #         ax1.tick_params(axis="x", labelsize=16)
-    #         ax1.tick_params(axis='y', labelcolor='tab:blue', labelsize=16)
-    #         ax2 = ax1.twinx()
-    #         ax2.plot(step_pcts, final_thetas, color='tab:red', linewidth=1.5, label=r"CAT $\theta$")
-    #         ax2.set_ylabel(r"CAT $\theta$", color='tab:red', fontsize=16)
-    #         ax2.tick_params(axis='y', labelcolor='tab:red', labelsize=16)
-    #         lines1, labels1 = ax1.get_legend_handles_labels()
-    #         lines2, labels2 = ax2.get_legend_handles_labels()
-    #         ax1.legend(lines1 + lines2, labels1 + labels2, fontsize=16)
-    #         plt.tight_layout()
-    #         plt.savefig(f"{output_dir}/law_curve.png", dpi=300, bbox_inches="tight")
-    #         plt.close()
-
-                    
-    #     results_dict[scenario][model] = {
-    #         "steps": steps,
-    #         "sub_ys": sub_ys,
-    #         "thetass": thetass,
-    #     }
-    
-    # final_results_dict = {k: dict(v) for k, v in results_dict.items()}
-    # with open(f"{output_dir}/result.pkl", "wb") as f:
-    # pickle.dump(final_results_dict, f)
+            pass_datk_gts = compute_pass_datk_gts(model_data)
+            pass_datk_irts = compute_pass_datk_irt(model_data, irt_probs)
+            mae = np.mean(np.abs(pass_datk_gts - pass_datk_irts))
+            n_samples = np.arange(1, model_data.shape[-1] + 1)
+            with plt.rc_context(bundles.icml2024(usetex=True, family="serif")):
+                plt.figure(figsize=(6, 6))
+                plt.plot(n_samples, pass_datk_gts, label="GT", linewidth=2)
+                plt.plot(n_samples, pass_datk_irts, label="IRT Est", linewidth=2, linestyle="--")
+                plt.xlabel("Number of Samples", fontsize=16)
+                plt.ylabel("Pass@k", fontsize=16)
+                plt.title(rf"{model}, {scen} (MAE = {mae:.2f})", fontsize=16)
+                plt.legend(fontsize=14)
+                plt.ylim(0,1)
+                plt.tick_params(axis="both", labelsize=14)
+                plt.tight_layout()
+                plt.savefig(f"{output_dir}/law_curve_{model}.png", dpi=300, bbox_inches="tight")
+                plt.close()
